560facb0b765c9bb13f1459c11d2e42b64255d0c
ketkar1981 <129646142+ketkar1981@users.noreply.github.com>
2023-08-14 13:32:13 +0000

    Dataset downloading from Figshare

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # content-moderation-workshop
+
+
---
+++ diff_thresholding.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import io
+import requests
+
+def get_dataset(key):
+    urls = {
+        "one-source":"https://figshare.com/ndownloader/files/26648885",
+        "hoax":"https://figshare.com/ndownloader/files/26648861"
+    }
+    response = requests.get(urls[key], allow_redirects=True)
+    df = pd.read_csv(io.StringIO(response.content.decode('utf-8')))
+    return df
