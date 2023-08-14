053ba93e9a19db649cd4599dc91530ae1f785764
ketkar1981 <129646142+ketkar1981@users.noreply.github.com>
2023-08-14 14:04:23 +0000

    Model building

--- diff_thresholding.py
+++ diff_thresholding.py
@@ -19,7 +19,6 @@ from sklearn.preprocessing import StandardScaler
 from xgboost import XGBClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_validate, train_test_split, GroupKFold
-from mlxtend.classifier import StackingCVClassifier
 from sklearn.feature_selection import SelectFromModel
 
 def get_dataset(key):
@@ -30,6 +29,42 @@ def get_dataset(key):
     response = requests.get(urls[key], allow_redirects=True)
     df = pd.read_csv(io.StringIO(response.content.decode('utf-8')))
     return df
+
+def train_test_split_grouped(df, test_size=0.1):
+    pageids = np.unique(df.index)
+    train_pageids, test_pageids =train_test_split(pageids,test_size=test_size)
+    train = df.loc[train_pageids]
+    train = shuffle(train)
+    test = df.loc[test_pageids]
+    test = shuffle(test)
+    group = train.index.values
+
+    return train, test, group
+
+def expand_scores(scores_dicts):
+    fit_time = {}; score_time = {}; accuracy = {}; precision = {}; recall = {}; f1 = {}; roc = {}
+
+    for modelname, scores in scores_dicts.items():
+        fit_time[modelname] = scores['fit_time'].mean()
+        score_time[modelname] = scores['score_time'].mean()
+        accuracy[modelname] = scores['test_accuracy'].mean()
+        precision[modelname] = scores['test_precision_macro'].mean()
+        recall[modelname] = scores['test_recall_macro'].mean()
+        f1[modelname] = scores['test_f1_weighted'].mean()
+        roc[modelname] = scores['test_roc_auc'].mean()
+
+    models_results = pd.DataFrame({
+        'Model'       : list(scores_dicts.keys()),
+        'Fitting time': list(fit_time.values()),
+        'Scoring time': list(score_time.values()),
+        'Accuracy'    : list(accuracy.values()),
+        'Precision'   : list(precision.values()),
+        'Recall'      : list(recall.values()),
+        'F1_score'    : list(f1.values()),
+        'AUC_ROC'     : list(roc.values()),
+        }, columns = ['Model', 'Fitting time', 'Scoring time', 'Accuracy', 'Precision', 'Recall', 'F1_score', 'AUC_ROC'])
+
+    return models_results
 
 def build_model(key, DROP_COLS=None):
     df = get_dataset(key)
@@ -75,3 +110,15 @@ def build_model(key, DROP_COLS=None):
     feature_importance = XGB_model.get_booster().get_score(importance_type='gain')
     model_results = expand_scores(model_scores)
     return model_results, feature_importance, XGB_model, X, y
+
+def model_training():
+  results = {}
+  template_names = ["one-source", "hoax"]
+  for template_name in template_names:
+    scores, feature_importance, model, X, y = build_model(template_name)
+    results[(template_name, "scores")] = scores
+    results[(template_name, "feature_importance")] = feature_importance
+    results[(template_name, "model")] = model
+    results[(template_name, "X")] = X
+    results[(template_name, "y")] = y
+  return results
